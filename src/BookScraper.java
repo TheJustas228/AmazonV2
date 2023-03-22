6d983fc48793acd20ce2929b8f894592708561d5
TheJustas228 <justassinkevicius228@gmail.com>
2023-03-22 11:25:12 +0200

    Update of misunderstanding

--- src/BookScraper.java
+++ src/BookScraper.java
@@ -3,17 +3,15 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class BookScraper {
 
     public static void main(String[] args) {
         String url = "https://www.knygos.lt/lt/knygos/zanras/publicistika/";
-        String targetTitle = "Baimės metai";
 
         try {
-            Map<String, String> bookDetails = scrapeBook(url, targetTitle);
+            Map<String, String> bookDetails = scrapeBook(url);
 
             if (bookDetails != null) {
                 System.out.println(bookDetails);
@@ -26,7 +24,7 @@ public class BookScraper {
         }
     }
 
-    public static Map<String, String> scrapeBook(String url, String targetTitle) throws IOException {
+    public static Map<String, String> scrapeBook(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
 
 //        System.out.print(doc.toString());
@@ -35,10 +33,10 @@ public class BookScraper {
 
         for (Element book : books) {
 
-            Element booklink = book.select("a").first();
+            Element bookLink = book.select("a").first();
 
-            String bookUrl = booklink.attr("href");
-//            System.out.println(bookUrl.toString().trim()); // veikia, outputtina knygu linkus visus
+            String bookUrl = bookLink.attr("href");
+            System.out.println(bookUrl.trim()); // veikia, outputtina knygu linkus visus
 
             Document bookDoc = Jsoup.connect(bookUrl).get();
 
@@ -46,32 +44,13 @@ public class BookScraper {
 
             String title = bookDoc.select("h1 span[itemprop=name]").text().trim();
 
-//            System.out.println(title.toString()); // veikia, outputins visu knygu pavadinimus puslayje
+            System.out.println(title); // veikia, outputins visu knygu pavadinimus puslayje
 
-            if (title.equalsIgnoreCase(targetTitle)) {
-//            if(false) {
-                Elements descriptionKeys = bookDoc.select(".Description-key-2Yw");
-                Elements descriptionValues = bookDoc.select(".Description-value-2Yw");
+            String bookCode = bookDoc.select(".about-product li").get(4).select("span[itemprop=isbn gtin13 sku]").text().trim();
 
-                Element bookCode = bookDoc.select(".about-product li").get(4);
+            System.out.println(bookCode); // check 6
 
-                System.out.println(bookCode); // check 6
-
-                Map<String, String> bookDetails = new HashMap<>();
-
-                bookDetails.put("title", title);
-
-                for (int i = 0; i < descriptionKeys.size(); i++) {
-                    String key = descriptionKeys.get(i).text().trim();
-                    String value = descriptionValues.get(i).text().trim();
-                    bookDetails.put(key, value);
-                }
-
-                return bookDetails;
-            }
-//            break;
         }
-
         return null;
     }
 }
